a9a0678e97c8578cdb13bdbab4ab12a16e30cafb
augustomauro <amtnet@hotmail.com>
2020-08-31 17:30:02 -0300

    menu.blade.php

--- resources/views/developer/layouts/menu.blade.php
+++ resources/views/developer/layouts/menu.blade.php
@@ -4,8 +4,8 @@
 <!-- <br>
 <a href="#"><i class="fas fa-user-alt"></i></i> My Profile</a>
 <br> -->
-<br>
+<!-- <br>
 <a href="{{ URL::previous() }}"><i class="fas fa-arrow-left"></i> Back</a>
-<br>
+<br> -->
 <br>
 <a href="/"><i class="fas fa-home"></i> Go Home</a>
